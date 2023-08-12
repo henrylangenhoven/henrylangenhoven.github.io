7becc5d28c58bb2ab9a140ba54e0ffa392e1ec9a
Henry Langenhoven <henrylangenhoven8@gmail.com>
2023-08-12 09:47:29 +0200

    extracted all sections into angular components

--- portfolio-app/src/app/app.module.ts
+++ portfolio-app/src/app/app.module.ts
@@ -7,9 +7,17 @@ import { HeroComponent } from './hero/hero.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { AboutComponent } from './main/about/about.component';
+import { FactsComponent } from './main/facts/facts.component';
+import { SkillsComponent } from './main/skills/skills.component';
+import { ResumeComponent } from './main/resume/resume.component';
+import { PortfolioComponent } from './main/portfolio/portfolio.component';
+import { ServicesComponent } from './main/services/services.component';
+import { TestimonialsComponent } from './main/testimonials/testimonials.component';
+import { ContactComponent } from './main/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, HeaderComponent, FooterComponent, MainComponent],
+  declarations: [AppComponent, HeroComponent, HeaderComponent, FooterComponent, MainComponent, AboutComponent, FactsComponent, SkillsComponent, ResumeComponent, PortfolioComponent, ServicesComponent, TestimonialsComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
