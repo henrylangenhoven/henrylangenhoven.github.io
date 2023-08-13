c670d2c75f9dc12a330abde589f09a84ffeeceee
Henry Langenhoven <henrylangenhoven8@gmail.com>
2023-08-13 12:09:15 +0200

    added main module and mocked components for tests

--- portfolio-app/src/app/app.component.spec.ts
+++ portfolio-app/src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MockComponents, MockModule } from 'ng-mocks';
+import { MainComponent } from './main/main.component';
+import { MainModule } from './main/main.module';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule, MockModule(MainModule)],
+      declarations: [AppComponent, MockComponents(HeaderComponent, HeroComponent, MainComponent, FooterComponent)],
     })
   );
 
@@ -20,12 +26,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('portfolio-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio-app app is running!');
   });
 });
--- portfolio-app/src/app/app.module.ts
+++ portfolio-app/src/app/app.module.ts
@@ -6,30 +6,11 @@ import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-import { AboutModule } from './main/about/about.module';
-import { ResumeModule } from './main/resume/resume.module';
-import { ServicesModule } from './main/services/services.module';
-import { ContactModule } from './main/contact/contact.module';
-import { PortfolioModule } from './main/portfolio/portfolio.module';
-import { FactsModule } from './main/facts/facts.module';
-import { TestimonialsModule } from './main/testimonials/testimonials.module';
-import { SkillsModule } from './main/skills/skills.module';
+import { MainModule } from './main/main.module';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, HeaderComponent, FooterComponent, MainComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AboutModule,
-    ResumeModule,
-    ServicesModule,
-    ContactModule,
-    PortfolioModule,
-    FactsModule,
-    TestimonialsModule,
-    SkillsModule,
-  ],
+  declarations: [AppComponent, HeroComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, MainModule],
   providers: [],
   bootstrap: [AppComponent],
 })
