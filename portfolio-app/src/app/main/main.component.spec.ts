df5c8e186168a7f69148f6151f4f0088cebe00e2
Henry Langenhoven <henrylangenhoven8@gmail.com>
2023-08-13 12:09:53 +0200

    added main module and mocked components for tests

--- portfolio-app/src/app/main/main.component.spec.ts
+++ portfolio-app/src/app/main/main.component.spec.ts
@@ -1,6 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainComponent } from './main.component';
+import { AboutModule } from './about/about.module';
+import { FactsModule } from './facts/facts.module';
+import { SkillsModule } from './skills/skills.module';
+import { MockComponents } from 'ng-mocks';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FactsComponent } from './facts/facts.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ServicesComponent } from './services/services.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 describe('MainComponent', () => {
   let component: MainComponent;
@@ -8,7 +20,20 @@ describe('MainComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MainComponent],
+      declarations: [
+        MainComponent,
+        MockComponents(
+          AboutComponent,
+          ContactComponent,
+          FactsComponent,
+          PortfolioComponent,
+          ResumeComponent,
+          ServicesComponent,
+          SkillsComponent,
+          TestimonialsComponent
+        ),
+      ],
+      imports: [AboutModule, FactsModule, SkillsModule],
     });
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
