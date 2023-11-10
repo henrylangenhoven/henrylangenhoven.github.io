9d3a17e96a733e5a0c728f1eb3807171d7fe0e98
Henry Langenhoven <henrylangenhoven8@gmail.com>
2023-11-10 15:19:05 +0100

    fix tests

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,6 +9,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [HeaderComponent],
     });
     fixture = TestBed.createComponent(HeaderComponent);
