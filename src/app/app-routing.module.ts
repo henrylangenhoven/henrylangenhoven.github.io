81a6b607e2c065b905b3f73d0bcfc10964b321bd
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-03-12 14:35:23 +0100

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
-  { path: 'home', component: HeroComponent },
-  { path: 'about', loadChildren: () => import('./main/about/about.module').then(m => m.AboutModule) },
-  { path: 'resume', loadChildren: () => import('./main/resume/resume.module').then(m => m.ResumeModule) },
-  { path: 'portfolio', loadChildren: () => import('./main/portfolio/portfolio.module').then(m => m.PortfolioModule) },
-  { path: 'services', loadChildren: () => import('./main/services/services.module').then(m => m.ServicesModule) },
-  { path: 'contact', loadChildren: () => import('./main/contact/contact.module').then(m => m.ContactModule) },
+  { path: '', component: HeroComponent },
+  { path: '#', redirectTo: '', pathMatch: 'full' },
+  { path: '#about', loadChildren: () => import('./main/about/about.module').then(m => m.AboutModule) },
+  { path: '#resume', loadChildren: () => import('./main/resume/resume.module').then(m => m.ResumeModule) },
+  { path: '#portfolio', loadChildren: () => import('./main/portfolio/portfolio.module').then(m => m.PortfolioModule) },
+  { path: '#services', loadChildren: () => import('./main/services/services.module').then(m => m.ServicesModule) },
+  { path: '#contact', loadChildren: () => import('./main/contact/contact.module').then(m => m.ContactModule) },
 ];
 
 @NgModule({
