cc1fb1baa919ed99aed936b048c73257cf8ecbad
Henry Langenhoven <henry.langenhoven@entrnce.com>
2025-02-11 14:19:54 +0100

    ran `ng generate @angular/core:route-lazy-loading` and removed remaining module routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroComponent } from './hero/hero.component';
 
 const routes: Routes = [
-  { path: '', component: HeroComponent },
+  { path: '', loadComponent: () => import('./hero/hero.component').then(m => m.HeroComponent) },
   { path: '#', redirectTo: '', pathMatch: 'full' },
-  { path: '#about', loadChildren: () => import('./main/about/about.module').then(m => m.AboutModule) },
-  { path: '#resume', loadChildren: () => import('./main/resume/resume.module').then(m => m.ResumeModule) },
-  { path: '#portfolio', loadChildren: () => import('./main/portfolio/portfolio.module').then(m => m.PortfolioModule) },
-  { path: '#services', loadChildren: () => import('./main/services/services.module').then(m => m.ServicesModule) },
-  { path: '#contact', loadChildren: () => import('./main/contact/contact.module').then(m => m.ContactModule) },
+  { path: '#about', loadChildren: () => import('./main/about/about.component').then(m => m.AboutComponent) },
+  { path: '#resume', loadChildren: () => import('./main/resume/resume.component').then(m => m.ResumeComponent) },
+  {
+    path: '#portfolio',
+    loadChildren: () => import('./main/portfolio/portfolio.component').then(m => m.PortfolioComponent),
+  },
+  {
+    path: '#services',
+    loadChildren: () => import('./main/services/services.component').then(m => m.ServicesComponent),
+  },
+  { path: '#contact', loadChildren: () => import('./main/contact/contact.component').then(m => m.ContactComponent) },
 ];
 
 @NgModule({
