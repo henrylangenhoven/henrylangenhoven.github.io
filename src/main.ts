39b18936d024c2bc89b3c31de99622c6d57618b9
Henry Langenhoven <henry.langenhoven.dev@gmail.com>
2025-11-29 08:51:24 +0100

    refactor: remove redundant modules and simplify bootstrap logic

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
-import { MainModule } from './app/main/main.module';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, MainModule)],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
 }).catch(err => console.error(err));
